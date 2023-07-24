f3a473edce46f1e418d3f3d2c0c45d307b289803
Thành Quý <thanhquy1105@gmail.com>
2023-07-25 00:42:43 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Bookings and Reservations
+
+This is the repository for my bookings and reservations project
+
+    Built in Go version 1.20.5
+
+Dependencies:
+
+    [chi router](github.com/go-chi/chi)
+    [alex edwards SCS session management](github.com/alexedwards/scs/v2)
+    [nosurf](github.com/justinas/nosurf)
+    [pgx](https://github.com/jackc/pgx)
+    [simple mail](https://github.com/xhit/go-simple-mail)
+    [Go validator](https://github.com/asaskevich/govalidator)
---
+++ cmd/web/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/thanhquy1105/bookings/pkg/config"
+	"github.com/thanhquy1105/bookings/pkg/handlers"
+	"github.com/thanhquy1105/bookings/pkg/render"
+)
+
+var portNumber = ":8080"
+var app config.AppConfig
+var session *scs.SessionManager
+
+func main() {
+
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("Cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	fmt.Println(fmt.Sprintln("Starting app on port ", portNumber))
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
