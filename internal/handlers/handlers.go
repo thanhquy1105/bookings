573ec0925108a8c6fcd47071fd00028cb9185804
Thành Quý <thanhquy1105@gmail.com>
2023-07-30 14:12:17 +0700

    setting up database functions inserting a reservation

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/thanhquy1105/bookings/internal/config"
 	"github.com/thanhquy1105/bookings/internal/driver"
@@ -61,11 +63,32 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	sd := r.Form.Get("start_date")
+	ed := r.Form.Get("end_date")
+
+	layout := "2006-01-02"
+	startDate, err := time.Parse(layout, sd)
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+	endDate, err := time.Parse(layout, ed)
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+
+	roomID, err := strconv.Atoi(r.Form.Get("room_id"))
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+
 	reservation := models.Reservation{
 		FirstName: r.Form.Get("first_name"),
 		LastName:  r.Form.Get("last_name"),
 		Phone:     r.Form.Get("phone"),
 		Email:     r.Form.Get("email"),
+		StartDate: startDate,
+		EndDate:   endDate,
+		RoomID:    roomID,
 	}
 
 	form := forms.New(r.PostForm)
@@ -83,6 +106,11 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 			Data: data,
 		})
 		return
+	}
+
+	err = m.DB.InsertReservation(reservation)
+	if err != nil {
+		helpers.ServerError(w, err)
 	}
 
 	m.App.Session.Put(r.Context(), "reservation", reservation)
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,5 +1,9 @@
 package repository
 
+import "github.com/thanhquy1105/bookings/internal/models"
+
 type DatabaseRepo interface {
 	AllUsers() bool
+
+	InsertReservation(res models.Reservation) error
 }
