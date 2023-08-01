6584d3e99964be2c29a988df3477d3b16774e35c
Thành Quý <thanhquy1105@gmail.com>
2023-08-01 18:33:55 +0700

    adding a migration for seeding rooms and seeding restrictions

---
+++ migrations/20230801111630_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name, created_at, updated_at) VALUES 
+('Major''s Suite'::character varying, '2023-07-30'::timestamp without time zone, '2023-07-30'::timestamp without time zone),
+('General''s Quaters'::character varying, '2023-07-30'::timestamp without time zone, '2023-07-30'::timestamp without time zone);
---
+++ migrations/20230801112705_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name, created_at, updated_at) VALUES 
+('Reservation'::character varying, '2023-07-01'::timestamp without time zone, '2023-07-01'::timestamp without time zone),
+('Owner Block'::character varying, '2023-07-01'::timestamp without time zone, '2023-07-01'::timestamp without time zone);
