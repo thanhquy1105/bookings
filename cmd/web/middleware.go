d94cc19ca81f7f6aa95adcb270a342d8a7a721c0
Thành Quý <thanhquy1105@gmail.com>
2023-08-07 02:42:39 +0700

    writing authentication middleware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/justinas/nosurf"
+	"github.com/thanhquy1105/bookings/internal/helpers"
 )
 
 func NoSurf(next http.Handler) http.Handler {
@@ -21,4 +22,16 @@ func NoSurf(next http.Handler) http.Handler {
 
 func SessionLoad(next http.Handler) http.Handler {
 	return session.LoadAndSave(next)
+}
+
+func Auth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !helpers.IsAuthenticated(r) {
+			session.Put(r.Context(), "error", "Log in first!")
+			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+			return
+		}
+		next.ServeHTTP(w, r)
+
+	})
 }
